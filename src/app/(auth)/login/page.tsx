b3e3fe2282257baa9707c37fef9deba4547f0a55
Oscar Joel Ramírez Manzano <oramirez0004@uta.edu.ec>
2025-11-04 12:43:26 -0500

    Add global styles and layout for Next.js application

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,6 @@
+import LoginView from "@/features/auth/presentation/views/loginView";
+
+
+export default function LoginPage() {
+  return <LoginView />;
+}
---
+++ src/features/auth/presentation/views/loginView.tsx
@@ -0,0 +1,4 @@
+
+export default function LoginView() {
+  return <div>Login View</div>;
+}
