45a67742634445c1e48ce5bd6d76801282940e87
OscarJRM <oramirez0004@uta.edu.ec>
2025-12-06 00:07:03 -0500

    feat: Add icon-collapsible sidebar mode and display nested menu items within tooltips.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
+                                                          async rewrites() {
     return [
       {
         source: '/backend-api/:path*',
